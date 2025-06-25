36ee84d5bd75d20bcc9258853f93f645298bc31a
Davison Mota <davimotagnr@gmail.com>
2025-06-25 11:29:37 -0300

    test partial prerendering

--- front-end/app-router/nextjs-dashboard/next.config.ts
+++ front-end/app-router/nextjs-dashboard/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    ppr: 'incremental' //'true' para todas as rotas
+  }
 };
 
 export default nextConfig;
