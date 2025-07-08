5705692cad989160f34bbb38e0b1e71875681578
Davison Mota <davimotagnr@gmail.com>
2025-07-08 20:16:43 -0300

    feat: edit invoice

--- front-end/app-router/nextjs-dashboard/next.config.ts
+++ front-end/app-router/nextjs-dashboard/next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    ppr: 'incremental' //'true' para todas as rotas
-  }
+  // experimental: {
+  //   ppr: 'incremental' //'true' para todas as rotas
+  // }
 };
 
 export default nextConfig;
